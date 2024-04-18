8c01e6f78756cb89d169820328811aff77df6bae
Shahriyar <chyshahriyar@gmail.com>
2024-04-18 23:33:41 +0600

    Some animation complete

--- src/Component/Estate/Estate.jsx
+++ src/Component/Estate/Estate.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Estateinfo from "../Estateinfo/Estateinfo";
-
+import AOS  from 'aos';
+import 'aos/dist/aos.css'
 
 const Estate = () => {
     const [estates, setestate] = useState([]);
@@ -12,10 +13,13 @@ const Estate = () => {
             .then(data =>  setestate(data));
     }
         , [])
+        useEffect(() => {
+            AOS.init();
+        },[])
 
     return (
         <div>
-            <div className="grid lg:grid-cols-3 grid-cols-1 gap-7 w-11/12 mx-auto">
+            <div className="grid lg:grid-cols-3 grid-cols-1 gap-7 w-11/12 mx-auto "data-aos="fade-up" data-aos-duration="2000" >
                 {
                     estates.map(estate => <Estateinfo estate={estate} key={estate.Id}></Estateinfo>)
                 }
