562b8f6dcf9e67bd2b222f441447ce0d25c429b0
Shahriyar <chyshahriyar@gmail.com>
2024-04-18 23:28:51 +0600

    firebase config done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB1gnc-Tqt-Nft_G7N1CWU0rI2_m2dxnew",
-  authDomain: "griho-abhijan.firebaseapp.com",
-  projectId: "griho-abhijan",
-  storageBucket: "griho-abhijan.appspot.com",
-  messagingSenderId: "13869109677",
-  appId: "1:13869109677:web:6a56b16167271a8c005124"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
